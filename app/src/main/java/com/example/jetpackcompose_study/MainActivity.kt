cf980f9d6719ad4f4d32e560b3235201f7821c4c
YeSeul <ysbsjh2464@naver.com>
2024-02-22 03:03:20 +0900

    :lipstick: style: add text list

--- app/src/main/java/com/example/jetpackcompose_study/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_study/MainActivity.kt
@@ -3,44 +3,60 @@ package com.example.jetpackcompose_study
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose_study.ui.theme.JetpackCompose_StudyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent {  // 레이아웃 정의
             JetpackCompose_StudyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(
+    modifier: Modifier = Modifier,  // 빈 수정자가 할당되는 수정자 매개변수를 포함
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+// 구성 가능한 함수: 함수가 내부에서 다른 @Composable 함수 호출 가능
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(modifier = modifier.fillMaxWidth().padding(24.dp)) {
+            Text(text = "Hello")
+            Text(text = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     JetpackCompose_StudyTheme {
-        Greeting("Android")
+        MyApp()
     }
 }

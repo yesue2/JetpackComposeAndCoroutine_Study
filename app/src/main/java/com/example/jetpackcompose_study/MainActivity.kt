7073e620b8fffbd744446ff9147aa0178a359449
YeSeul <ysbsjh2464@naver.com>
2024-02-23 17:44:29 +0900

    :zap: refactor: modify greeting list column to lazycolumn

--- app/src/main/java/com/example/jetpackcompose_study/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_study/MainActivity.kt
@@ -24,6 +24,8 @@ import androidx.compose.material3.Button
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 
 class MainActivity : ComponentActivity() {
@@ -77,10 +79,10 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }

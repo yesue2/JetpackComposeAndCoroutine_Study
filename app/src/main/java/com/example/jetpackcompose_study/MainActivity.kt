71267e8e53a5d0a23907a439f34a390d8c8ef35b
YeSeul <ysbsjh2464@naver.com>
2024-02-23 16:51:26 +0900

    :sparkles: feat: add state hoisting pattern

--- app/src/main/java/com/example/jetpackcompose_study/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_study/MainActivity.kt
@@ -19,6 +19,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcompose_study.ui.theme.JetpackCompose_StudyTheme
 import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 
 class MainActivity : ComponentActivity() {
@@ -35,6 +40,43 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,  // 빈 수정자가 할당되는 수정자 매개변수를 포함
+) {
+    // by : = 대신 사용, 매번 .value를 입력할 필요가 없도록 해주는 속성
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {  // onBoarding이 보여질 때 (true일 때)
+            // OnboardingScreen 실행해 Button이 클릭되면 false로 바꿔주기
+            OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
+        } else {  // onBoarding이 보여지지 않을 때 (false일 때)
+            Greetings()  // 다른 화면 출력
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier,
+    onContinueClicked: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
     Column(modifier = modifier.padding(vertical = 4.dp)) {
@@ -47,26 +89,43 @@ fun MyApp(
 // 구성 가능한 함수: 함수가 내부에서 다른 @Composable 함수 호출 가능
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+     var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { expanded.value = !expanded.value },
+                onClick = { expanded = !expanded },
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    JetpackCompose_StudyTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    JetpackCompose_StudyTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
@@ -74,6 +133,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     JetpackCompose_StudyTheme {
-        MyApp()
+        Greetings()
     }
 }
